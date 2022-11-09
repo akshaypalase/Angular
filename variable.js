92bb9f1cc46e95a412a1854e4d36935d89d5c9b2
akshaypalase <akshaypalase3@gmail.com>
2022-11-09 23:17:20 +0530

    updated
    updated

---
+++ variable.js
@@ -0,0 +1,17 @@
+var n1 = 10;
+console.log(n1);
+var s1 = "you are the creater of your destiny";
+var s2 = "Powerfull people comes from the powerfull places.";
+console.log(s1);
+console.log(s2);
+var a1 = {
+    productID: 1,
+    ProductName: 'iPhone',
+    productPrice: 25000
+};
+console.log(a1);
+// homodenous array 
+var arr1 = ['Angular', 'React', 'NodeJS', 'java'];
+// heterogenous array 
+var arr2 = ['Angular', 123456, true];
+console.log(arr2);
---
+++ variable.ts
@@ -0,0 +1,23 @@
+var n1: number = 10;
+console.log(n1);
+var s1: string="you are the creater of your destiny";
+var s2: string="Powerfull people comes from the powerfull places.";
+console.log(s1);
+console.log(s2);
+
+var a1: any = {
+    productID: 1,
+    ProductName: 'iPhone',
+    productPrice: 25000
+}
+
+console.log(a1);
+
+// homodenous array 
+var arr1 : Array<string> = ['Angular', 'React', 'NodeJS', 'java'];
+
+// heterogenous array 
+var arr2 : Array<any> = ['Angular', 123456, true];
+
+
+console.log(arr2);
